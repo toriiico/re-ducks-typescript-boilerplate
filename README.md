9f18d0f6e6f245a4695ddf54085c16d722fb0750
toriiico <ggrn.danfim@gmail.com>
2019-03-13 14:39:47 +0900

    chore: Organize files.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # re-ducks-typescript-material-ui-todos
+
 react + redux + typescript + material-ui + typesafe-actions and other awesome Todo app!
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import logo from "./logo.svg"
+import "./App.css"
 
 class App extends Component {
   render() {
@@ -11,18 +11,13 @@ class App extends Component {
           <p>
             Edit <code>src/App.tsx</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
             Learn React
           </a>
         </header>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
