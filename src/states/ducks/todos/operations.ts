4840e293b404585e9534c9222bfd227ca827d848
toriiico <ggrn.danfim@gmail.com>
2019-03-14 00:12:06 +0900

    refactor: omission dispatch

--- src/states/ducks/todos/operations.ts
+++ src/states/ducks/todos/operations.ts
@@ -1,18 +1,10 @@
-import { Dispatch } from "redux"
 import actions from "./actions"
 
-const addTodo = (text: string) => (dispatch: Dispatch) => {
-  return dispatch(actions.addTodo(text))
-}
+const addTodo = (text: string) => actions.addTodo(text)
 
-const setVisibilityFilter = (filter: string) => (dispatch: Dispatch) => {
-  console.log(filter)
-  return dispatch(actions.setVisibilityFilter(filter))
-}
+const setVisibilityFilter = (filter: string) => actions.setVisibilityFilter(filter)
 
-const toggleTodo = (id: string) => (dispatch: Dispatch) => {
-  return dispatch(actions.toggleTodo(id))
-}
+const toggleTodo = (id: string) => actions.toggleTodo(id)
 
 export default {
   addTodo,
