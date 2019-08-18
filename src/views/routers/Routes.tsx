5f90dbe994eadcf8364fb5bd72429a97888859da
Ryosuke Narita <rynar@synspective.com>
2019-08-18 18:38:51 +0900

    feat: Apply redirect when called unexpected path.

--- src/views/routers/Routes.tsx
+++ src/views/routers/Routes.tsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import { Route, Switch } from "react-router-dom"
+import { Redirect, Route, Switch } from "react-router-dom"
 
 import Home from "../pages"
 import Todo from "../pages/todo"
@@ -16,6 +16,8 @@ const FComponent: React.FC<Props> = (props: Props) => {
 
       {/* Todo app */}
       <Route exact={true} path="/todo" render={(_props: any) => <Todo {..._props} />} />
+
+      <Redirect to="/" />
     </Switch>
   )
 }
